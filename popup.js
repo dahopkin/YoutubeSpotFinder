a0e70623b55f8f00b8bacde71962cd590bd68f4e
dahopkin <dahopkin2@gmail.com>
2018-01-25 19:25:06 -0500

    Added popup.html file and backing js.
    Added popup.html file and backing js for when the Youtube JumpTo button
    is clicked. The js just sends messages that the content script will act
    upon.

---
+++ popup.js
@@ -0,0 +1,19 @@
+$(function () {
+    function sendActionAsMessageFromCurrentTab(actionToSend) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: actionToSend });
+        });
+    }
+    $('#goTo1-4').click(function () {
+        sendActionAsMessageFromCurrentTab("goTo1-4")
+    });
+    $('#goTo2-4').click(function () {
+        sendActionAsMessageFromCurrentTab("goTo2-4")
+    });
+    $('#goTo3-4').click(function () {
+        sendActionAsMessageFromCurrentTab("goTo3-4")
+    });
+    $('#goTo30').click(function () {
+        sendActionAsMessageFromCurrentTab("goTo30")
+    });
+});
