0fc68d315bfc579400f437844f880daa55cefbe3
dahopkin <dahopkin2@gmail.com>
2018-03-01 16:39:26 -0500

    Got rid of console test messages.

--- eventPage.js
+++ eventPage.js
@@ -5,12 +5,10 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         });
     }
 });
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     if(changeInfo && changeInfo.status == "complete"){
-        console.log("Tab updated: " + tab.url);
-
         chrome.tabs.sendMessage(tabId, {data: tab}, function(response) {
-            console.log(response);
         });
 
     }
