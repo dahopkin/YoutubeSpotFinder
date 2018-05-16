4f92c3a17550be23f26a200826b79976f205d1a7
dahopkin <dahopkin2@gmail.com>
2018-05-16 01:03:31 -0400

    Added "view all bookmarks" button to popup.
    Exporting/importing the data is now in options, and the popup has a button for the page that views all bookmarks.

--- popup.js
+++ popup.js
@@ -1,25 +1,8 @@
 $(function () {
     
-    $("#export-bookmarks").on("click.export", function(){
-        //export code (with modifications) comes from:
-        //https://stackoverflow.com/questions/23160600/chrome-extension-local-storage-how-to-export
-        try {
-            chrome.storage.sync.get(null, function(items) { // null implies all items
-                // Convert object to a string.
-                var result = JSON.stringify(items);
-            
-                // Save as file
-                var url = 'data:application/json;base64,' + btoa(result);
-                chrome.downloads.download({
-                    url: url,
-                    filename: 'ysp-bookmarks.json'
-                });
-            });
-                
-            } catch (error) {
-                console.log("error saving");
-                alert(JSON.stringify(error));
-                return true;
-            }
-    });
+    $("#view-bookmarks").on("click.view",function(){
+        let allBookmarksURL = chrome.runtime.getURL('all-bookmarks.html')
+        chrome.tabs.create({url: allBookmarksURL});
+        return false;
+      } );
 });
